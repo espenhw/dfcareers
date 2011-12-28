7ca5f290b7356a897f6c86633ec4d135a295c1b4
Espen Wiborg <espenhw@grumblesmurf.org>
2011-12-28 12:18:16 +0100

    Report errors 'sensibly'

--- src/org/grumblesmurf/df/careers/Main.java
+++ src/org/grumblesmurf/df/careers/Main.java
@@ -8,7 +8,6 @@ import org.w3c.dom.NodeList;
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.IOException;
@@ -40,10 +39,8 @@ public class Main
     public static void readDwarvesFrom(File file) {
         dwarvesList.clear();
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = null;
         try {
-            dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
+            Document doc = dbFactory.newDocumentBuilder().parse(file);
             NodeList creatures = doc.getElementsByTagName("Creature");
             List<Dwarf> newDwarves = new LinkedList<Dwarf>();
             for (int i = 0; i < creatures.getLength(); i++) {
@@ -67,16 +64,22 @@ public class Main
     public static void displayDwarf(int index) {
         Dwarf dwarf = dwarvesList.getElementAt(index);
         HTMLDocument document = (HTMLDocument) form.dwarfInfo.getDocument();
-        javax.swing.text.Element body = document.getElement("body");
         try {
-            document.setOuterHTML(body, "<body id='body'></body>");
-            body = document.getElement("body");
+            javax.swing.text.Element body = newBodyElement(document);
             dwarf.renderTo(body);
         } catch (BadLocationException e) {
-            e.printStackTrace();  // FIXME: Do something
+            reportError(e);
         } catch (IOException e) {
-            e.printStackTrace();  // FIXME: Do something
+            reportError(e);
         }
+    }
+
+    private static javax.swing.text.Element newBodyElement(HTMLDocument document)
+        throws BadLocationException, IOException {
+        javax.swing.text.Element body = document.getElement("body");
+        document.setOuterHTML(body, "<body id='body'></body>");
+        body = document.getElement("body");
+        return body;
     }
 
     private static class DwarvesListModel extends AbstractListModel
